845fc8dc57500f9fb1f1dda344b10b9c499bdf28
Hemant1803 <hshemantsharma445@gmail.com>
2023-09-25 01:15:37 +0530

    logical_program1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Logical_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select the Program You Want To Run");
+            Console.WriteLine("\n1.Square of Number");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Squareroot squareroot = new Squareroot();
+                   
+                    break;
+            }
+        }
+    }
+}
---
+++ Squareroot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Logical_Programs
+{
+    internal class Squareroot
+    {
+         public Squareroot() 
+        {
+            Console.WriteLine("Enter a Number You Want To Squareroot");
+            double num = Convert.ToDouble(Console.ReadLine());
+            double squareroot = Math.Sqrt(num);
+            Console.WriteLine($"The Square Root of the {num} is :-" + squareroot);
+        }
+    }
+}
